76078a85a895c007cd3f1e449a5c198e6c38846e
omeyr <omeyr@omeyr.home>
2023-06-21 15:19:50 +0200

    nvlle methode ip

--- scriptmidi.py
+++ scriptmidi.py
@@ -13,7 +13,8 @@ chemin = '/'.join(chemin)
 
 print('ip récupérée de {}'.format(chemin))
 
-adresseip = open(chemin).readlines()[0]
+adresseip = open(chemin).readlines()[0].split(' ')[0]
+print(adresseip)
 port = 5054
 
 print('lancement...')
